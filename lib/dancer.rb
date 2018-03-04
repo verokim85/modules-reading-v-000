595913cd1ec924ab71430d0d339b36dcbbd28003
Veronica Kim <vero.a.kim@gmail.com>
2018-03-04 04:48:51 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,7 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer < FancyDance
+class Dancer 
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
